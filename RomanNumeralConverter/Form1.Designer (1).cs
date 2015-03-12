69e76c567278874e42dc6b24de21f6d73aa7e93e
Kiely <kitaco1028@gmail.com>
2015-03-12 18:08:31 -0400

    Roman Numeral Converter
    Convert numbers 1-10 to Roman Numerals

---
+++ RomanNumeralConverter/Form1.Designer (1).cs
@@ -0,0 +1,135 @@
+﻿namespace RomanNumeralConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.convertedLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(13, 13);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(237, 13);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Enter a number to convert it to Roman Numerals:";
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(256, 10);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(100, 20);
+            this.inputTextbox.TabIndex = 1;
+            this.inputTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(256, 48);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // convertedLabel
+            // 
+            this.convertedLabel.AutoSize = true;
+            this.convertedLabel.Location = new System.Drawing.Point(108, 54);
+            this.convertedLabel.Name = "convertedLabel";
+            this.convertedLabel.Size = new System.Drawing.Size(138, 13);
+            this.convertedLabel.TabIndex = 3;
+            this.convertedLabel.Text = "Converted Roman Numeral:";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(76, 86);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(157, 86);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(238, 86);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 129);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.convertedLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputTextbox);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "Form1";
+            this.Text = "Roman Numeral Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label convertedLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ RomanNumeralConverter/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomanNumeralConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            // make variable from inputTextbox
+            var input = int.Parse(inputTextbox.Text);
+
+            // check if input is less than 0 or greater than 10, if not - show error
+            if (input <= 0 || input > 10) {
+                MessageBox.Show("Please enter a number between 1 and 10");
+            }
+            else  {
+                // switch input numbers for roman numerals, display roman numeral in outputLabel
+                switch (input)
+                {
+                    case 1:
+                        outputLabel.Text = "I";
+                        break;
+                    case 2:
+                        outputLabel.Text = "II";
+                        break;
+                    case 3:
+                        outputLabel.Text = "III";
+                        break;
+                    case 4:
+                        outputLabel.Text = "IV";
+                        break;
+                    case 5:
+                        outputLabel.Text = "V";
+                        break;
+                    case 6:
+                        outputLabel.Text = "VI";
+                        break;
+                    case 7:
+                        outputLabel.Text = "VII";
+                        break;
+                    case 8:
+                        outputLabel.Text = "VIII";
+                        break;
+                    case 9:
+                        outputLabel.Text = "IX";
+                        break;
+                    case 10:
+                        outputLabel.Text = "X";
+                        break;
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // clear textbox and label
+            inputTextbox.Text = String.Empty;
+            outputLabel.Text = String.Empty;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // close form
+            this.Close();
+        }
+    }
+}
